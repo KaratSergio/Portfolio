3b982e4f7b31a3b03d111ed4a08d26ff7a1cbd9a
Sergio <137483700+KaratSergio@users.noreply.github.com>
2024-06-06 11:17:37 +0300

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,17 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {};
+
+// export default nextConfig;
+
+const isProd = process.env.NODE_ENV === "production";
+
+const nextConfig = {
+  basePath: isProd ? "/Portfolio" : "",
+  assetPrefix: isProd ? "/Portfolio/" : "",
+  images: {
+    loader: "imgix",
+    path: isProd ? "https://KaratSergio.github.io/Portfolio/" : "/",
+  },
+};
 
 export default nextConfig;
