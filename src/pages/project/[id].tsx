d0222786d2350b417f3f11ff4601f9f609020d18
Sergio <137483700+KaratSergio@users.noreply.github.com>
2024-06-06 17:06:40 +0300

    Update [id].tsx

--- src/pages/project/[id].tsx
+++ src/pages/project/[id].tsx
@@ -22,6 +22,11 @@ export const getStaticProps: GetStaticProps = async ({ params }) => {
 const ProjectDetail = ({ project }: { project: Project }) => {
   if (!project) return <div>Project not found</div>;
 
+  const isLocal = process.env.NODE_ENV === "development";
+  const image_path = isLocal
+    ? `/images/${project.name}.png`
+    : `../images/${project.name}.png`;
+
   return (
     <div
       className="container bg-slate-200 dark:bg-zinc-950 mx-auto px-4 pb-4 relative overflow-x-auto scrollbar-thin"
@@ -40,7 +45,7 @@ const ProjectDetail = ({ project }: { project: Project }) => {
         <div className="md:w-1/2">
           <Image
             className="rounded-lg"
-            src={project.image_path}
+            src={image_path}
             alt={project.name}
             layout="responsive"
             height={150}
