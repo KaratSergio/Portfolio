1010e11b2ebb1de0605ae58f7d32325181f4cb64
Sergio <137483700+KaratSergio@users.noreply.github.com>
2024-06-06 15:31:24 +0300

    Update projectDetailUtils.ts

--- src/pages/project/projectDetailUtils.ts
+++ src/pages/project/projectDetailUtils.ts
@@ -1,14 +1,14 @@
-import { GetStaticPaths, GetStaticProps } from "next";
-import { projects } from "../../data/data";
+// import { GetStaticPaths, GetStaticProps } from "next";
+// import { projects } from "../../data/data";
 
-export const getStaticPaths: GetStaticPaths = async () => {
-  const paths = projects.map((project) => ({
-    params: { id: project.name },
-  }));
-  return { paths, fallback: false };
-};
+// export const getStaticPaths: GetStaticPaths = async () => {
+//   const paths = projects.map((project) => ({
+//     params: { id: project.name },
+//   }));
+//   return { paths, fallback: false };
+// };
 
-export const getStaticProps: GetStaticProps = async ({ params }) => {
-  const project = projects.find((p) => p.name === params?.id);
-  return { props: { project } };
-};
+// export const getStaticProps: GetStaticProps = async ({ params }) => {
+//   const project = projects.find((p) => p.name === params?.id);
+//   return { props: { project } };
+// };
